fa50b5ce0d20dde10a9c8c72678e249e33ee23d8
Rita Patrick <rita.patrick1007@gmail.com>
2018-03-31 12:53:51 -0500

    updated blogs.js to ES6 and added XHR

--- javascript/blogs.js
+++ javascript/blogs.js
@@ -1,26 +1,42 @@
 
-function writeToDom(stringToPrint, divId){
+const writeToDom = (stringToPrint, divId) => {
   //take string and innerHTML to the div
   var myDiv = document.getElementById(divId);
   // console.log("myDiv", myDiv);
   myDiv.innerHTML += stringToPrint;
 }
 
-function createBlogPosts(){
-  for(var i=0; i<blogs.length; i++){
+const createBlogPosts = (blogsArray) => {
+    
+  for(var i=0; i<blogsArray.length; i++){
     var blogsString = "";
-    blogsString += "<article class='content-container'><h3>" + blogs[i].title + "</h3>";
-    blogsString += "<p>" + blogs[i].date + "</p>";
-    blogsString += "<p>" + blogs[i].post + "</p>";
+    blogsString += "<article class='content-container'><h3>" + blogsArray[i].title + "</h3>";
+    blogsString += "<p>" + blogsArray[i].date + "</p>";
+    blogsString += "<p>" + blogsArray[i].post + "</p>";
     blogsString += "</article>";
     writeToDom(blogsString, "my-projects");
   }
 }
-createBlogPosts();
 
-/* <h2>Week 1 of NSS</h2>
-    <p>I think the most interesting topic this week was GIT and github. GIT is challenging to learn at first because of it unique concept. However, once I practiced using GIT, I understood how it makes "life" easier for developers. I wish that before I started NSS, I had used GIT and github for practice on my own. On the other hand, GIT commands should be used carefully, especially git rm command!</p>
-    <h2>Week 3 of NSS</h2>
-    <p>Week 3 of NSS started with “introduction” of JavaScript. I put it in quotes because I felt that this introduction to JavaScript accelerated to more advance topics such as looping through arrays on the second day of class after we started JavaScript. Moreover, there was a homework of array challenges! This is where I wrote and solved my very FIRST JavaScript challenges! Every time I solved a challenge, I felt rush of energy! I think I am addicted to coding...</p>
-    <h2>Week 4 of NSS</h2>
-    <p>Week 4 of NSS. I learned Objects and Functions. And of course, more homework challenges on Objects and Functions!!! I think it’s getting easier to solve challenges and it’s less time consuming. I spend a lot of time reading MDN and researching to complete challenges using different approaches.</p> */
+function executeThisCodeIfXHRFails () {
+  console.log("error");
+}
+
+function executeThisCodeAfterFileLoaded () {
+  const data = JSON.parse(this.responseText);
+  createBlogPosts(data.blogs);
+  console.log(data.blogs);
+}
+
+const startApplication = () => {
+  let myRequest = new XMLHttpRequest();
+  myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+  myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+  myRequest.open("GET","../db/projects.json");
+  myRequest.send();
+  // console.log(myRequest);
+};
+
+startApplication ();
+
+
